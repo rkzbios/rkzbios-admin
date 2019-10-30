660cbe0decb1724873e0b0a8804d4653c53c4b14
roho <robert.hofstra@knowlogy.nl>
2019-10-30 14:09:37 +0100

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+https://web.archive.org/web/20190312155833/http://www.rkzbios.nl/Spider-Man-Into-the-Spider-Verse.htm
+
+https://graphene-python.org/
+https://relay.dev/docs/en/graphql-server-specification.html
+
+https://wagtail.io/blog/getting-started-with-wagtail-and-graphql/
+
+http://docs.wagtail.io/en/v2.6.2/topics/streamfield.html#structural-block-types
+
+https://nec.is/writing/graphql-with-next-js-and-apollo/
+https://github.com/necccc/nextjs-apollo-graphql
+https://github.com/humanmade/react-oembed-container
+
---
+++ rkzbios_site/api/schema.py
@@ -0,0 +1,30 @@
+from __future__ import unicode_literals
+import graphene
+from graphene_django import DjangoObjectType
+from home.models import FilmPage, Filmdates
+from api import graphene_wagtail
+from django.db import models
+
+class FilmPageNode(DjangoObjectType):
+    class Meta:
+        model = FilmPage
+        fields = ['id', 'title','director','country','body','film_dates']
+
+
+class FilmdatesType(DjangoObjectType):
+    class Meta:
+        model = Filmdates
+
+class Query(graphene.ObjectType):
+    films = graphene.List(FilmPageNode)
+    film = graphene.Field(FilmPageNode, film_id=graphene.String())
+
+    @graphene.resolve_only_args
+    def resolve_films(self):
+        return FilmPage.objects.all()
+
+    def resolve_film(self, info, film_id):
+        # Querying a single question
+        return FilmPage.objects.get(pk=film_id)
+
+schema = graphene.Schema(query=Query)
