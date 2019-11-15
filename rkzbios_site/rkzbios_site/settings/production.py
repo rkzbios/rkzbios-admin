ac8de9f8498bdcd3a49ea5b715287c3abde83cd9
roho <robert.hofstra@knowlogy.nl>
2019-11-15 12:02:40 +0100

    added configurations

--- rkzbios_site/rkzbios_site/settings/production.py
+++ rkzbios_site/rkzbios_site/settings/production.py
@@ -2,6 +2,16 @@ from .base import *
 
 DEBUG = False
 
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'z*lj!h50z8z71$2yc$pe$f$ucl57p)vi(ejd8om$#543ko+q0p'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.mysql',
