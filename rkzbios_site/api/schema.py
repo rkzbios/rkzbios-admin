46d9392d6729db55b60798a11127a2a8b630f8e2
roho <robert.hofstra@knowlogy.nl>
2019-11-28 11:19:59 +0100

    Little refactoring renamed FilmPage to MoviePage

--- rkzbios_site/api/schema.py
+++ rkzbios_site/api/schema.py
@@ -1,30 +1,41 @@
 from __future__ import unicode_literals
+
+from datetime import date
 import graphene
 from graphene_django import DjangoObjectType
-from home.models import FilmPage, Filmdates
+from home.models import MoviePage, Moviedate
 from api import graphene_wagtail
 from django.db import models
 
-class FilmPageNode(DjangoObjectType):
+class MoviePageNode(DjangoObjectType):
     class Meta:
-        model = FilmPage
-        fields = ['id', 'title','director','country','body','film_dates','filmPoster','filmBack']
+        model = MoviePage
+        fields = ['id', 'title','director','country','body','movieDates','moviePoster','movieBackDrop']
 
 
 class FilmdatesType(DjangoObjectType):
     class Meta:
-        model = Filmdates
+        model = Moviedate
 
 class Query(graphene.ObjectType):
-    films = graphene.List(FilmPageNode)
-    film = graphene.Field(FilmPageNode, film_id=graphene.String())
+    movies = graphene.List(MoviePageNode)
+    movie = graphene.Field(MoviePageNode, film_id=graphene.String())
+    currentMovie = graphene.Field(MoviePageNode)
 
     @graphene.resolve_only_args
-    def resolve_films(self):
-        return FilmPage.objects.all()
+    def resolve_movies(self):
+        today = date.today()
 
-    def resolve_film(self, info, film_id):
+        return MoviePage.objects.filter(movieDates__date__gte=today).order_by('movieDates__date')
+
+    def resolve_movie(self, info, film_id):
         # Querying a single question
-        return FilmPage.objects.get(pk=film_id)
+        return MoviePage.objects.get(pk=film_id)
+
+
+    @graphene.resolve_only_args
+    def resolve_currentFilm(self):
+        return MoviePage.objects.get(pk=3)
+
 
 schema = graphene.Schema(query=Query)
--- rkzbios_site/rkzbios_site/api.py
+++ rkzbios_site/rkzbios_site/api.py
@@ -1,15 +1,147 @@
+from datetime import date
+from collections import OrderedDict
+from django.conf.urls import url
+
+from rest_framework.serializers import ListSerializer
+from rest_framework.utils.serializer_helpers import ReturnList
+
 from wagtail.api.v2.endpoints import PagesAPIEndpoint
 from wagtail.api.v2.router import WagtailAPIRouter
+from wagtail.api.v2.serializers import PageSerializer
 from wagtail.images.api.v2.endpoints import ImagesAPIEndpoint
 from wagtail.documents.api.v2.endpoints import DocumentsAPIEndpoint
 
+from wagtail.api.v2.filters import FieldsFilter, OrderingFilter, \
+    SearchFilter, BaseFilterBackend
+
+from home.models import MoviePage
+
 # Create the router. "wagtailapi" is the URL namespace
 api_router = WagtailAPIRouter('wagtailapi')
+
+
+class CurrentActiveMoveFilter(BaseFilterBackend):
+    """
+    Implements current active filter.
+    """
+
+    def filter_queryset(self, request, queryset, view):
+        if 'currentActive' in request.GET:
+            today = date.today()
+
+            queryset = queryset.filter(movieDates__date__gte = today).order_by('movieDates__date')
+
+        return queryset
+
+
+# class CustomListSerializer(ListSerializer):
+#
+#     @property
+#     def data(self):
+#         ret = super().data
+#         return ReturnList(ret, serializer=self)
+#
+# class MyMoviePageSerializer(PageSerializer):
+#
+#     class Meta:
+#         list_serializer_class = CustomListSerializer
+
+class MoviePagesAPIEndpoint(PagesAPIEndpoint):
+    #base_serializer_class = MyMoviePageSerializer  DOESNOT WORK META FIELD IS IGNORED BY WAGTAIL
+
+    filter_backends = [
+        FieldsFilter,
+        CurrentActiveMoveFilter,
+        OrderingFilter,
+        SearchFilter,
+    ]
+
+    known_query_parameters = frozenset([
+        'limit',
+        'offset',
+        'fields',
+        'order',
+        'search',
+        'search_operator',
+        'currentActive',
+
+        # Used by jQuery for cache-busting. See #1671
+        '_',
+
+        # Required by BrowsableAPIRenderer
+        'format',
+    ])
+
+    def remove_duplicates(self, data):
+        cleaned = OrderedDict()
+
+        for item in data:
+            _id = item['id']
+            if not _id in cleaned:
+                cleaned[_id] = item
+        cleaned_list = [item for item in cleaned.values()]
+        return cleaned_list
+
+
+    def listing_view(self, request):
+        queryset = self.get_queryset()
+        self.check_query_parameters(queryset)
+        queryset = self.filter_queryset(queryset)
+        queryset = self.paginate_queryset(queryset)
+        serializer = self.get_serializer(queryset, many=True)
+        data = serializer.data
+
+        removed_duplicates_data =  self.remove_duplicates(data)
+        self.paginator.total_count = len(removed_duplicates_data)
+        return self.get_paginated_response(removed_duplicates_data)
+
+
+    def get_queryset(self):
+        request = self.request
+
+        queryset = MoviePage.objects.all()
+        # Get live pages that are not in a private section
+        queryset = queryset.public().live()
+
+        # Filter by site
+        if request.site:
+            queryset = queryset.descendant_of(request.site.root_page, inclusive=True)
+        else:
+            # No sites configured
+            queryset = queryset.none()
+
+        return queryset
+
+
+    def get_object(self):
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+        value = self.kwargs[lookup_url_kwarg]
+        if value == "_current":
+            queryset = self.get_queryset()
+            today = date.today()
+            base = queryset.filter(movieDates__date__gte=today).order_by('movieDates__date').first()
+
+        else:
+            base = super().get_object()
+        return base.specific
+
+    @classmethod
+    def get_urlpatterns(cls):
+        """
+        This returns a list of URL patterns for the endpoint
+        """
+        return [
+            url(r'^$', cls.as_view({'get': 'listing_view'}), name='listing'),
+            #url(r'^(?P<pk>\d+)/$', cls.as_view({'get': 'detail_view'}), name='detail'),
+            url(r'^(?P<pk>[^/]+)/$', cls.as_view({'get': 'detail_view'}), name='detail'),
+            url(r'^find/$', cls.as_view({'get': 'find_view'}), name='find'),
+        ]
 
 # Add the three endpoints using the "register_endpoint" method.
 # The first parameter is the name of the endpoint (eg. pages, images). This
 # is used in the URL of the endpoint
 # The second parameter is the endpoint class that handles the requests
 api_router.register_endpoint('pages', PagesAPIEndpoint)
+api_router.register_endpoint('moviePages', MoviePagesAPIEndpoint)
 api_router.register_endpoint('images', ImagesAPIEndpoint)
 api_router.register_endpoint('documents', DocumentsAPIEndpoint)
